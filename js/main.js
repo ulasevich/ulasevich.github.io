9d5d6e26fcab5a2078bfee7cc4933802965522a1
ulasevich <aleksandr@spellsystems.com>
2019-11-27 16:09:24 +0300

    comment

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//Глобальный объект App сайта
+var App = {
+    win: $(window),
+	doc: $(document),
+	is_mobile: detectmob(), // браузер мобильный или нет
+	ms_ie: /(MSIE|Trident\/|Edge\/)/i.test(navigator.userAgent), // Браузер - ie или нет
+};
+
+// определение мобильного браузера
+function detectmob() { 
+	if(navigator.userAgent.match(/Android/i)
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+	)
+		{
+			return true;
+		}
+	else {
+		return false;
+	}
+}
+
+
+// различный дополнительный функционал
+App.Other = function () {
+	if (App.is_mobile) {
+		$('html').addClass('is_mobile');
+	}
+};
+
+
+$(document).ready(function(){
+});
---
+++ js/panel.js
@@ -0,0 +1,13 @@
+//Глобальный объект (панель) для разделов портфолио
+var Panel = {
+    win: $(window),
+	doc: $(document),
+	is_mobile: detectmob(), // браузер мобильный или нет
+	ms_ie: /(MSIE|Trident\/|Edge\/)/i.test(navigator.userAgent), // Браузер - ie или нет
+};
+
+
+
+$(document).ready(function(){
+	
+});
